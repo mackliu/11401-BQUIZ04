3824bdce7f789521d6143f9bc46a4b76c44d9e4f
mackliu <macklun@gmail.com>
2025-08-12 14:36:14 +0800

    完成驗證碼檢查

--- front/login.php
+++ front/login.php
@@ -17,8 +17,30 @@
     <tr>
         <td class="tt ct">驗證碼</td>
         <td class="pp">
+            <?php
+            $a=rand(10,99);
+            $b=rand(10,99);
+            $_SESSION['ans']=$a+$b;
+            echo $a . " + ". $b . " = ";
+
+            ?>
             <input type="text" name="chk" id="chk">
         </td>
     </tr>
  </table>
-<div class="ct"><button>確認</button></div>
+<div class="ct">
+    <button onclick="login()">確認</button>
+</div>
+
+<script>
+function login(){
+    let chk=$("#chk").val();
+    $.get("./api/chkAns.php",{chk},(res)=>{
+        if(parseInt(res)==1){
+            alert("驗證成功");
+        }else{
+            alert("驗證失敗");
+        }
+    })
+}    
+</script>
