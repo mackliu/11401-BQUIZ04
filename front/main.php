1e1c53ae637f2286a5dc7d1ec8d59d3efb4b489e
mackliu <macklun@gmail.com>
2025-08-25 11:32:33 +0800

    完成商品陳列及商品詳細資料

--- front/main.php
+++ front/main.php
@@ -21,10 +21,39 @@ if($type!=0){
 
 <h2><?=$nav;?></h2>
 
-<?php
-foreach($items as $item){
-    echo $item['name'];
-    echo "<br>";
-}
+<?php  foreach($items as $item): ?>
+<div style="display:flex;width:65%;margin:5px auto;height:150px;">
+    <div class="pp ct" style="padding:20px">
+        <a href="?do=detail&id=<?=$item['id'];?>">
+        <img src="./image/<?=$item['img'];?>" style="width:150px;height:100px;">
+        </a>
+    </div>
+    <div>
+        <table style="height:100%;">
+            <tr>
+                <td class="tt ct"><?=$item['name'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>價錢:<?=$item['price'];?>
+                <a href="?do=buycart&id=<?=$item['id'];?>&qt=1">
+                <img src="./icon/0402.jpg" style="float:right">
+                </a>
+            </td>
+            </tr>
+            <tr class="pp">
+                <td>規格:<?=$item['spec'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    簡介:<?=mb_substr($item['intro'],0,20);?>...
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
 
-?>
+
+
+
+
+<?php endforeach; ?>
