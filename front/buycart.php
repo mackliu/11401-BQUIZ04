52b9cd5bfd4704d82ef4b747f324a538f7023883
mackliu <macklun@gmail.com>
2025-08-25 14:15:08 +0800

    建立購物車session

--- front/buycart.php
+++ front/buycart.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+ 
+if(isset($_GET['id'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+
 if(!isset($_SESSION['login'])){
     to("?do=login");
 }
@@ -7,3 +12,14 @@ if(!isset($_SESSION['login'])){
 
 
 <h2 class="ct"><?=$_SESSION['login'];?>的購物車</h2>
+
+<?php
+if(isset($_SESSION['cart']) && count($_SESSION['cart'])>0){
+    dd($_SESSION['cart']);
+}else{
+    
+    echo "購物車是空的";
+}    
+    
+
+?>
