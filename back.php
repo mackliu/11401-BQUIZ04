a7971716dd77131e9a5bbdda7eb76c9d1c79eb2a
mackliu <macklun@gmail.com>
2025-08-25 09:02:35 +0800

    將分類顯示在前台首頁

--- back.php
+++ back.php
@@ -56,7 +56,7 @@ $super_pr=unserialize($super['pr']);
         ?>	
         	        </div>
         <div id="bottom" style="line-height:70px; color:#FFF; background:url(./icon/bot.png);" class="ct">
-        	頁尾版權 :        </div>
+        	<?=$Bot->find(1)['bottom'];?>       </div>
     </div>
 
 </body></html>
