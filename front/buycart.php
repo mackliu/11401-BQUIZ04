f394c27493353e01840191b929244510257cf15f
mackliu <macklun@gmail.com>
2025-08-25 14:31:07 +0800

    在購物車顯示商品列表

--- front/buycart.php
+++ front/buycart.php
@@ -9,13 +9,54 @@ if(!isset($_SESSION['login'])){
 }
 ?>
 
-
-
 <h2 class="ct"><?=$_SESSION['login'];?>的購物車</h2>
 
 <?php
 if(isset($_SESSION['cart']) && count($_SESSION['cart'])>0){
-    dd($_SESSION['cart']);
+?>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+    <?php
+    foreach($_SESSION['cart'] as $id => $qt):
+        $item=$Item->find($id);
+    ?>
+    <tr class="pp ct">
+        
+        <td><?=$item['no'];?></td>
+        <td><?=$item['name'];?></td>
+        <td><?=$qt;?></td>
+        <td><?=$item['stock'];?></td>
+        <td><?=$item['price'];?></td>
+        <td><?=$item['price']*$qt;?></td>
+        <td>
+            <img src="./icon/0415.jpg" alt="">
+        </td>
+    </tr>
+    <?php 
+    endforeach;
+    ?>
+</table>
+
+
+<div  class='ct'>
+    <a href="index.php">
+        <img src="./icon/0411.jpg" alt="">
+    </a>
+    <a href="?do=checkout">
+        <img src="./icon/0412.jpg" alt="">
+    </a>
+</div>
+
+<?php    
 }else{
     
     echo "購物車是空的";
